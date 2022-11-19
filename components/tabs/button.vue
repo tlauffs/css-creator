3ccf49dbee345f99cbb9eeaab86c4d1baca52378
unknown <timlauffs@gmail.com>
2022-11-19 18:40:15 +0100

    worked on button component

--- components/tabs/button.vue
+++ components/tabs/button.vue
@@ -4,7 +4,12 @@ const counter = useState('counter', () => 100);
 const roundedBtnRange = ref(0);
 const colorBtnInput = ref('#023b59');
 const textColorBtnInput = ref('#f6ebf4');
-const snippitExpand = ref({open: false, show: 'Show', chevron: 'less' })
+const snippitExpand = ref({ open: false, show: 'Show', chevron: 'less' });
+const buttonNameInput = ref('');
+
+const buttonList = reactive({});
+const selectdBtn = ref('');
+
 
 const primaryButton = reactive({
     backgroundColor: colorBtnInput,
@@ -12,27 +17,55 @@ const primaryButton = reactive({
     border: 'none',
     padding: '0.75rem 1rem',
     cursor: 'pointer',
-    borderRadius: computed(() => (roundedBtnRange.value / 10) + 'rem')
+    borderRadius: '0.5rem'
 })
 
-const kebabize = str => {
-    return str.split('').map((letter, idx) => {
-        return letter.toUpperCase() === letter
-            ? `${idx !== 0 ? '-' : ''}${letter.toLowerCase()}`
-            : letter;
-    }).join('');
+function camelToKebab(input) {
+    return input.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
 }
 
-function toggleCodeSnippet(){
-    if(snippitExpand.value.open){
+function kebabToCamel(input) {
+    return input.replace(/-([a-z])/g, function (g) {
+        return g[1].toUpperCase();
+    });
+}
+
+function toggleCodeSnippet() {
+    if (snippitExpand.value.open) {
         snippitExpand.value.open = false;
         snippitExpand.value.show = 'Show';
         snippitExpand.value.chevron = 'less';
-    }else{
+    } else {
         snippitExpand.value.open = true;
         snippitExpand.value.show = 'Hide';
         snippitExpand.value.chevron = 'More';
     }
+}
+
+function createButton() {
+    if (buttonNameInput.value === '' || !(/^[a-zA-Z0-9-]+$/.test(buttonNameInput.value))) {
+        alert("Please enter a valid name / Names may only contain letters, numbers and -'s");
+        return;
+    }
+    var name = kebabToCamel('btn-' + buttonNameInput.value);
+    if (buttonList[name]) {
+        alert('A button with this name already exists.');
+        return;
+    }
+    buttonList[name] = {
+        backgroundColor: '#4cbfa6',
+        color: '#000000',
+        border: 'none',
+        padding: '0.75rem 1rem',
+        cursor: 'pointer',
+        borderRadius: '0rem'
+    };
+    /*const test = computed(() => (roundedBtnRange.value / 10) + 'rem');*/
+    selectdBtn.value = camelToKebab(name);
+}
+
+function updateBorder(e) {
+    buttonList[kebabToCamel(selectdBtn.value)].borderRadius = e.target.value / 10 + 'rem';
 }
 
 </script>
@@ -40,74 +73,107 @@ function toggleCodeSnippet(){
 <template>
     <div class="flex-horizontal w100">
         <div class="flex-vertical menu-wrapper">
-            <div class="style-group">
-                <h3 class="menu-title">Type</h3>
-                <div class="input-flex m1-t">
-                    <label>
-                        <input type="radio" name="btnType">
-                        Primary
-                    </label>
-                    <label>
-                        <input type="radio" name="btnType">
-                        Secondary
-                    </label>
-                    <label>
-                        <input type="radio" name="btnType">
-                        Link
-                    </label>
-                </div>
+            <div>
+                <label for="buttons" class="m1-t">
+                    <p>Create New Button</p>
+                </label>
+                <input class="text-input w100" type="text" placeholder="New Button Name" v-model="buttonNameInput">
+                <p class="small">* all buttons will be named 'btn-{name}'</p>
+                <button @click="createButton()" class="btn-primary btn-create w100">
+                    <p>Create</p>
+                </button>
             </div>
-            <div class="style-group">
-                <h3 class="menu-title">Colors</h3>
-                <div class="input-flex">
-                    <label for="colorpicker">
-                        <p>Background Color:</p>
-                    </label>
-                    <input v-model="colorBtnInput" type="color">
+            <label for="buttons" class="m1-t">
+                <p>Your Buttons:</p>
+            </label>
+            <select v-model="selectdBtn" name="buttons" required>
+                <option value="" disabled>Create or choose a button</option>
+                <option v-for="(value, key) in buttonList">{{ camelToKebab(key) }}</option>
+            </select>
+            <div v-if="selectdBtn">
+                <div class="style-group">
+                    <h3 class="menu-title">Type</h3>
+                    <div class="input-flex m1-t">
+                        <label>
+                            <input type="radio" name="btnType">
+                            Primary
+                        </label>
+                        <label>
+                            <input type="radio" name="btnType">
+                            Secondary
+                        </label>
+                        <label>
+                            <input type="radio" name="btnType">
+                            Link
+                        </label>
+                    </div>
                 </div>
-                <div class="input-flex">
-                    <label for="colorpicker">
-                        <p>Text Color:</p>
-                    </label>
-                    <input v-model="textColorBtnInput" type="color">
+                <div class="style-group">
+                    <h3 class="menu-title">Colors</h3>
+                    <div class="input-flex">
+                        <label for="colorpicker">
+                            <p>Background Color:</p>
+                        </label>
+                        <input v-model="buttonList[kebabToCamel(selectdBtn)].backgroundColor" type="color">
+                    </div>
+                    <div class="input-flex">
+                        <label for="colorpicker">
+                            <p>Text Color:</p>
+                        </label>
+                        <input v-model="buttonList[kebabToCamel(selectdBtn)].color" type="color">
+                    </div>
                 </div>
-            </div>
-            <div class="style-group">
-                <h3 class="menu-title">Styles </h3>
-                <div class="input-flex">
-                    <span>Rounded:</span>
-                    <div>
-                        <input v-model="roundedBtnRange" class="range-slider" type="range" min="1" max="30">
+                <div class="style-group">
+                    <h3 class="menu-title">Styles </h3>
+                    <div class="input-flex">
+                        <span>Rounded:</span>
+                        <div>
+                            <input @change="updateBorder" value="0" class="range-slider" type="range" min="1" max="20">
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+
+
         <div class="main-content flex-middle flex-grow">
-            <div>
-                <button :style="primaryButton" class="primary-button w100">
-                    Primary
-                </button>
+            <div class="w100">
+                <div v-for="(value, key) in buttonList" class="flex-middle"
+                    :class="{ 'test': kebabToCamel(selectdBtn) === key }">
+                    <button :style="value" class="primary-button m1-tb">
+                        {{ camelToKebab(key) }}
+                    </button>
+                </div>
             </div>
+
             <div class="code-snippet" :class="{ showcode: snippitExpand.open }">
                 <button @click="toggleCodeSnippet()" class="btn-link show-code-btn m1-b">
-                    <p> {{snippitExpand.show}} Code <span class="material-symbols-outlined icon-code-snippet">
-                            expand_{{snippitExpand.chevron}}
+                    <p> {{ snippitExpand.show }} Code <span class="material-symbols-outlined icon-code-snippet">
+                            expand_{{ snippitExpand.chevron }}
                         </span></p>
                 </button>
-                <p class="link-color">.primary-button {
-                <div v-for="(value, key) in primaryButton">
-                    <span class="m1-l"><span class="primary-color">{{ kebabize(key) }}</span> : <span
+
+                <p class="link-color">.{{ selectdBtn }}</p>
+                <p>{</p>
+                <div v-for="(value, key) in buttonList[kebabToCamel(selectdBtn)]">
+                    <span class="m1-l"><span class="primary-color">{{ camelToKebab(key) }}</span>: <span
                             class="secondary-color">{{
                             value}};</span></span>
                 </div>
                 <p>}</p>
-                </p>
             </div>
         </div>
+
+
+
     </div>
 </template>
   
 <style>
+.test {
+    background-color: rgb(0 0 0/25%);
+}
+
 .main-content {
     flex-direction: column;
     justify-content: space-between;
@@ -137,15 +203,18 @@ function toggleCodeSnippet(){
     transform: translateY(0.4rem);
 }
 
-.show-code-btn{
+.show-code-btn {
     opacity: 0.9;
     transform: scale(0.9);
 }
 
-.show-code-btn:hover{
+.show-code-btn:hover {
     opacity: 1;
     transform: scale(1);
     background-color: transparent;
 }
 
+.btn-create {
+    margin-top: 0.3rem;
+}
 </style>
