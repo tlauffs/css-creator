bd7401f84b0932e873056b4630404a220d53f476
Your Name <timlauffs@gmail.com>
2022-12-18 21:46:40 +0100

    worked on coloor component

--- components/componentMenu.vue
+++ components/componentMenu.vue
@@ -1,85 +1,75 @@
 <script setup>
-const tabDefault = shallowRef(resolveComponent('tabsDefault'));
-const ColorComp = shallowRef(resolveComponent('tabsColor'));
-const ButtonComp = shallowRef(resolveComponent('tabsButton'));
+const tabDefault = shallowRef(resolveComponent("tabsDefault"));
+const ColorComp = shallowRef(resolveComponent("tabsColor"));
+const ButtonComp = shallowRef(resolveComponent("tabsButton"));
 const tab = tabDefault;
 
 const styleList = reactive({});
 
-function addStyles(key, name, value) {
-    if (name === '' || !(/^[a-zA-Z0-9-]+$/.test(name))) {
-        alert('pls enter a valid name');
+function addStyles(key, name, value, action) {
+  if (action === "add") {
+    if (name === "" || !/^[a-zA-Z0-9-]+$/.test(name)) {
+      alert("pls enter a valid name");
+      return;
+    }
+    if (key === "colors") {
+      if (!name.startsWith("--")) {
+        alert("Color name must start with '--'");
         return;
+      }
     }
-
-    if (key === 'colors') {
-        if(!(name.startsWith("--"))){
-            alert("Color name must start with '--'")
-            return;
-        }
+    if (!Object.hasOwn(styleList, key)) {
+      styleList[key] = new Map();
     }
-
-    if (!(Object.hasOwn(styleList, key))) {
-        styleList[key] = new Map();
+    if (styleList[key].has(name)) {
+      alert("A color with this name already exists.");
     }
-    styleList[key].set(name, value)
+    styleList[key].set(name, value);
+    return;
+  }
+  if (action === "delete") {
+    styleList[key].delete(name);
+    return;
+  }
 }
-
 </script>
 <template>
-    <div class="flex-horizontal">
-        <div style="z-index:1" class="flex-vertical menu-wrapper">
-            <div>
-                <h3 class="menu-title">
-                    Helpers
-                </h3>
-            </div>
-            <button @click="tab = 'layout'" class="menu-item btn-link">
-                <p>Layout
-                </p>
-                <span class="material-symbols-outlined menu-icon">
-                    chevron_right
-                </span>
-            </button>
-            <button @click="tab = ColorComp" class="menu-item btn-link">
-                <p>Colors
-                </p>
-                <span class="material-symbols-outlined menu-icon">
-                    chevron_right
-                </span>
-            </button>
-            <div>
-                <h3 class="menu-title">
-                    Components
-                </h3>
-            </div>
-            <button @click="tab = ButtonComp" class="menu-item btn-link">
-                <p>Buttons
-                </p>
-                <span class="material-symbols-outlined menu-icon">
-                    chevron_right
-                </span>
-            </button>
-            <button @click="tab = 'headers'" class="menu-item btn-link">
-                <p>Headers
-                </p>
-                <span class="material-symbols-outlined menu-icon">
-                    chevron_right
-                </span>
-            </button>
-            <button @click="tab = 'cards'" class="menu-item btn-link">
-                <p>Cards
-                </p>
-                <span class="material-symbols-outlined menu-icon">
-                    chevron_right
-                </span>
-            </button>
-        </div>
-        <div class="flex-grow">
-            <component :is="tab" @my-add-styles-event="addStyles" :styleListProp='styleList' />
-        </div>
+  <div class="flex-horizontal">
+    <div style="z-index: 1" class="flex-vertical menu-wrapper">
+      <div>
+        <h3 class="menu-title">Helpers</h3>
+      </div>
+      <button @click="tab = 'layout'" class="menu-item btn-link">
+        <p>Layout</p>
+        <span class="material-symbols-outlined menu-icon"> chevron_right </span>
+      </button>
+      <button @click="tab = ColorComp" class="menu-item btn-link">
+        <p>Colors</p>
+        <span class="material-symbols-outlined menu-icon"> chevron_right </span>
+      </button>
+      <div>
+        <h3 class="menu-title">Components</h3>
+      </div>
+      <button @click="tab = ButtonComp" class="menu-item btn-link">
+        <p>Buttons</p>
+        <span class="material-symbols-outlined menu-icon"> chevron_right </span>
+      </button>
+      <button @click="tab = 'headers'" class="menu-item btn-link">
+        <p>Headers</p>
+        <span class="material-symbols-outlined menu-icon"> chevron_right </span>
+      </button>
+      <button @click="tab = 'cards'" class="menu-item btn-link">
+        <p>Cards</p>
+        <span class="material-symbols-outlined menu-icon"> chevron_right </span>
+      </button>
     </div>
+    <div class="flex-grow">
+      <component
+        :is="tab"
+        @my-add-styles-event="addStyles"
+        :styleListProp="styleList"
+      />
+    </div>
+  </div>
 </template>
-<style>
-
-</style>
+<style></style>
--- components/tabs/color.vue
+++ components/tabs/color.vue
@@ -1,76 +1,111 @@
 <script setup>
-const emit = defineEmits(['myAddStylesEvent']);
-const currentColor = ref('#ffffff');
-const currentName = ref('');
+const emit = defineEmits(["myAddStylesEvent"]);
+const currentColor = ref("#ffffff");
+const currentName = ref("");
 
-const props = defineProps(['styleListProp']);
+const props = defineProps(["styleListProp"]);
 const styleList = props.styleListProp;
 
 function addColor() {
-    emit("myAddStylesEvent", 'colors', currentName.value, currentColor.value);
+  emit("myAddStylesEvent", "colors", currentName.value, currentColor.value, "add");
 }
+
+function deleteColor(name){
+    emit("myAddStylesEvent", "colors", name, "", "delete");
+}
+
 </script>
 <template>
-    <div>
-        <div>{{ styleList }}</div>
-        <div class="flex-vertical main-color-wrapper">
-            <h2>Add Colors you want to use on your App</h2>
-        </div>
-        <div v-for="color in styleList.colors" class="input-flex color-flex" :style="{ backgroundColor: currentColor }">
-            <div class="hover-text">
-                <p>
-                    <span class="material-symbols-outlined">
-                        info
-                    </span>
-                </p>
-
-                <span class="tooltip-text" id="bottom">
-                    <p class="m1-b">The enterd name will be the name of your CSS custom property. You will be able to
-                        use it
-                        anywhere in your webapp and to style components in this tool.</p>
-                    <p class="m1-b">Custom property names must start with '--'</p>
-                    <p class="m1-b">Typical CSS color variable format: </p>
-                    <p class="m1-b"> --{ name }-color<br> exp: --primary-color</p>
-                    <p>
-                        For more info <a target="_blank"
-                            href="https://developer.mozilla.org/en-US/docs/Web/CSS/Using_CSS_custom_properties">click
-                            here.</a>
-                    </p>
-                </span>
-            </div>
-            <div class="flex-middle gap">
-                <p>color</p>
-                <div>
-                    <input class="text-input" type="text" v-model="currentName" placeholder="Enter Color Name">
-                </div>
-                <div>
-                    <input type="color" v-model="currentColor">
-                </div>
-                <button class="btn-white" @click="addColor">
-                    <p class="flex-middle">
-                        <span class="material-symbols-outlined m1-rs">
-                            add
-                        </span>Add Color
-                    </p>
-                </button>
-            </div>
-        </div>
+  <div>
+    <div>{{ styleList }}</div>
+    <div class="flex-vertical main-color-wrapper">
+      <h2>Add Colors you want to use on your App</h2>
     </div>
+    <!-- user colors -->
+    <div
+      v-for="color in styleList.colors"
+      class="input-flex color-flex border-bottom"
+      :style="{ backgroundColor: color[1] }"
+    >
+      <div class="flex-middle gap color-label">
+        <p>{{ color[0] }} : {{ color[1] }}</p>
+        <button class="btn-link btn-delete" @click="deleteColor(color[0])">Delete</button>
+      </div>
+    </div>
+    <!-- add new color -->
+    <div
+      class="input-flex color-flex"
+      :style="{ backgroundColor: currentColor }"
+    >
+      <div class="hover-text">
+        <p>
+          <span class="material-symbols-outlined"> info </span>
+        </p>
+        <span class="tooltip-text" id="bottom">
+          <p class="m1-b">
+            The enterd name will be the name of your CSS custom property. You
+            will be able to use it anywhere in your webapp and to style
+            components in this tool.
+          </p>
+          <p class="m1-b">Custom property names must start with '--'</p>
+          <p class="m1-b">Typical CSS color variable format:</p>
+          <p class="m1-b">
+            --{ name }-color<br />
+            exp: --primary-color
+          </p>
+          <p>
+            For more info
+            <a
+              target="_blank"
+              href="https://developer.mozilla.org/en-US/docs/Web/CSS/Using_CSS_custom_properties"
+              >click here.</a
+            >
+          </p>
+        </span>
+      </div>
+      <div class="flex-middle gap">
+        <div>
+          <input
+            class="text-input"
+            type="text"
+            v-model="currentName"
+            placeholder="Enter Color Name"
+          />
+        </div>
+        <div>
+          <input type="color" v-model="currentColor" />
+        </div>
+        <button class="btn-white" @click="addColor">
+          <p class="flex-middle">
+            <span class="material-symbols-outlined m1-rs"> add </span>Add Color
+          </p>
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style>
 .main-color-wrapper {
-    padding: 2rem;
+  padding: 2rem;
 }
 
 .color-flex {
-    justify-content: center;
-    gap: 1rem;
-    padding: 2rem 0;
+  justify-content: center;
+  gap: 1rem;
+  padding: 2rem 0;
 }
 
 .contrast-color {
-    filter: invert(1) saturate(0.5);
-    transition: all 200ms ease-in-out;
+  filter: invert(1) saturate(0.5);
+  transition: all 200ms ease-in-out;
+}
+.color-label {
+  background-color: var(--back-color);
+  padding: 0.4rem 0.8rem;
+  border-radius: 0.2rem;
+}
+.btn-delete{
+    padding: 0.5rem 0.8rem;
 }
 </style>
